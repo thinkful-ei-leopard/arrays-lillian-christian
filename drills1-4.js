9b1753ab7eedf21a2f8ec4d99f118818c7cca586
Lillian Burnside <lillian.burnside@gmail.com>
2020-04-01 10:56:37 -0500

    added Array and Memory class

---
+++ drills1-4.js
@@ -0,0 +1,68 @@
+'use strict';
+const memory= require('./memory');
+
+class Array {
+  construtor() {
+    this.length = 0;
+    this.ptr = memory.allocate(this.length);
+  }
+
+  push(value) {
+    this._resize(this.length + 1);
+    memory.set(this.ptr + this.length, value);
+    this.length++;
+  }
+
+  get(index) {
+    if (index < 0 || index >= this.length) {
+      throw new Error('Index error');
+    }
+    return memory.get(this.ptr + index);
+  }
+
+  pop() {
+    if (this.length === 0) {
+      throw new Error('Index error');
+    }
+    const value = memory.get(this.prt + this.length - 1);
+    this.length--;
+    return value;
+  }
+
+  insert(index, value) {
+    if (index < 0 || index >= this.length) {
+      throw new Error('Index error');
+    }
+
+    if (this.length >= this._capacity) {
+      this._resize((this.length + 1) * Array.SIZE_RATIO);
+    }
+
+    memory.copy(this.ptr + index + 1, this.ptr + index, this.length - index);
+    memory.set(this.ptr + index, value);
+    this.length++;
+  }
+
+  remove(index) {
+    if(index < 0 || index >= this.length) {
+      throw new Error('Index error');
+    }
+    
+    memory.copy(this.ptr + index, this.ptr + index +1, this.length - index - 1);
+    this.length--;
+  }
+
+  _resize(size) {
+    const oldPtr = this.ptr;
+    this.ptr = memory.allocate(size);
+    if (this.ptr === null) {
+      throw new Error('Out of memory');
+    }
+    memory.copy(this.ptr, oldPtr, this.length);
+    memory.free(oldPtr);
+  }
+}
+
+Array.SIZE_RATIO = 10;
+
+module.exports = Array
